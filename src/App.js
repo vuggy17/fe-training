e18fe927e1ece1f88fefcedee74537fba185936a
vuggy17 <vuggy17@gmail.com>
2020-12-24 11:21:43 +0700

    init react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Typography from "@material-ui/core/Typography";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Typography className="h1" component="h1" variant="h2">
+        haha
+        <span> get things done</span>
+      </Typography>
+    </div>
+  );
+};
+
+export default App;
